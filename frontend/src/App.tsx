f4c95dca699a0932896b2726dacf61f836d4308c
Axaba <lefevre.natacha60@gmail.com>
2025-06-21 02:23:42 +0200

    fix: Paramètres Tailwind V4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-6">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6">
       <h1 className="text-4xl text-green-500 font-bold underline text-center">
-        ✅ Tailwind fonctionne 🎉
+        ✅ Blindtest 🎉
       </h1>
       <p className="text-xl text-gray-700">
         Test de taille et couleur
       </p>
-      <div className="hidden text-pink-600 text-4xl">force inclusion</div>
     </div>
   );
 }
