2f4753e5201da5f04aca6122657f7c01411bc031
Axaba <lefevre.natacha60@gmail.com>
2025-10-14 22:33:46 +0200

    fix: Couleur footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return <footer
 
-   className="w-full max-w-screen p-4 md:items-center text-1xl fixed bottom-0 text-white bg-purple-500/80 text-center">
+   className="w-full max-w-screen p-4 md:items-center text-1xl fixed bottom-0 text-white bg-purple-700 text-center">
    <span>© 2025 Natacha Lefèvre. En cours de développement. Signalez-moi le moindre bug sur : </span>
         <a
           href="mailto:contact@axaba.fr"
